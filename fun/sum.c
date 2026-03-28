7a8424214eac3260e6c3fa2a2e8af495fbca10c4
SugZiuL <sugziul@gmail.com>
2026-03-28 00:39:36 -0300

    Adicionando algumas pastas e maios outros códiguinhos

---
+++ fun/sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 10, b = 20, c = 20;
+
+    int sum;
+
+    sum = a + b + c;
+
+    printf("A soma total entre a, b e c = %i", sum);
+
+    return 0;
+}
